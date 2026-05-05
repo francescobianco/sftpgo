7b1069545083b0634d19f2f572f4bd8c573836e8
francesco <info.francescobianco@gmail.com>
2026-05-05 16:07:28 +0200

    Updated at 2026-05-05 16:07:28

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ cp -f docker-compose.override.yml.example docker-compose.override.yml
 docker compose stop
 rm -fr tunnel.pub
 touch tunnel.pub
-
+chmod 777 tunnel.pub
 ls -la
 
 docker compose up -d --force-recreate
